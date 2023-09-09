2a787b6c9dbfa7ea6aa05aa9eea127c679470f7d
Михаил Сохатский <sohatsckij.misha@yandex.ru>
2023-09-09 16:48:51 +0500

    Добавил карточки на главную страницу

--- app/src/main/java/com/example/applicationbuilding/MainActivity.java
+++ app/src/main/java/com/example/applicationbuilding/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.applicationbuilding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -12,6 +14,8 @@ import android.widget.ImageButton;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextInputLayout textInputLayout;
@@ -19,17 +23,21 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton mapButton;
 
+    private RecyclerView.Adapter adapterTable;
+    private RecyclerView recyclerViewTable;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initRecyclerView();
 
         textInputLayout = findViewById(R.id.menu_drop);
         autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
 
-        String [] cities = {"Уфа", "Москва", "СПБ"}; //ToDo: Подгузить возможные города
+        String[] cities = {"Уфа", "Москва", "СПБ"}; //ToDo: Подгузить возможные города
 
         ArrayAdapter<String> citiesAdapter = new ArrayAdapter<>(MainActivity.this,
                 R.layout.drop_down_item, cities);
@@ -46,6 +54,28 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, MapsActivity.class));
 
         });
+
+
+    }
+
+    private void initRecyclerView() {
+        ArrayList<ItemDomain> itemsArray = new ArrayList<>();
+
+        itemsArray.add(new ItemDomain("цукп цукпацкупук пук пукпу кпукп\n укп укпукп ук пук пукп ",
+                "wefwegf `123 d", "23r23r23r", 13123123, "pic1"));
+
+        itemsArray.add(new ItemDomain("цукп цукпацкупук пук пукпу кпукп\n укп укпукп ук пук пукп ",
+                "wefwegf `123 d", "23gergerge", 43252133, "pic2"));
+
+        itemsArray.add(new ItemDomain("цукп цукпацкупук пук пукпу кпукп\n укп укпукп ук пук пукп ",
+                "wefwegf `123 d", "234g432g4rte", 35241232, "pic1"));
+
+        recyclerViewTable = findViewById(R.id.view_table);
+        recyclerViewTable.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false));
+
+        adapterTable = new ItemsAdapter(itemsArray);
+        recyclerViewTable.setAdapter(adapterTable);
 
 
     }
